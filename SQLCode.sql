d49c8db1fcb2c103fd191d8aff0d81c76a9fa4cc
Ryan Bell <rcb04@uw.edu>
2025-03-17 14:42:30 -0700

    Selected Columns First, Last name and Email From Customer

--- SQLCode.sql
+++ SQLCode.sql
@@ -1,7 +1,16 @@
 -- !preview conn=DBI::dbConnect(RSQLite::SQLite(), "WSDA_Music.db")
+-- This is my first SQL query
+/*
+Created by: Ryan Bell
+Created: 3/17/2025
+Description: Starting practice with SQL using WSDA_Music.db
+Also connected to GitHub, SQL-Practices
+*/
 
-SELECT * 
-FROM Genre 
+
+SELECT FirstName, LastName, Email 
+FROM Customer
+ 
 
 
 
