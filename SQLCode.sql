2f615ac189615968cd680778cf948272798e39cd
Ryan Bell <rcb04@uw.edu>
2025-03-19 11:26:30 -0700

    Separating Text pt. 2

--- SQLCode.sql
+++ SQLCode.sql
@@ -13,7 +13,9 @@ SELECT
 FirstName,
 LastName,
 Address,
-FirstName || ' ' || LastName || ' ' || Address || ', ' || City || ' ' || State || ' ' || PostalCode AS [Mailing Address]
+FirstName || ' ' || LastName || ' ' || Address || ', ' || City || ' ' || State || ' ' || PostalCode AS [Mailing Address],
+LENGTH(PostalCode),
+SUBSTR(PostalCode, 1,5) AS [5 Digit Postal Code]
 From
 Customer
 Where
